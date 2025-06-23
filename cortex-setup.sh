bdeab0f5e732bc95e7b44e42fa77bc31ca61053f
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-06-23 11:08:46 -0400

    Add files via upload

---
+++ cortex-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone git@github.com:jrabinovitz-ideal/cortex.git /opt/
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-9.0.0-x86_64.rpm
+sudo rpm -vi filebeat-9.0.0-x86_64.rpm
+mv /opt/cortex/filebeat.yml /etc/filebeat/
+(crontab -l 2>/dev/null || true; echo "5 * * * * /usr/bin/python3 /opt/fetch-cortex-alerts.py") | crontab -

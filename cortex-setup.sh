c52e2ea5e2fae95a02124d4f936e4be2b5cea700
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-06-24 09:47:56 -0400

    Update cortex-setup.sh

--- cortex-setup.sh
+++ cortex-setup.sh
@@ -4,3 +4,5 @@ curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-9.0.0-
 sudo rpm -vi filebeat-9.0.0-x86_64.rpm
 mv /opt/filebeat.yml /etc/filebeat/
 (crontab -l 2>/dev/null || true; echo "5 * * * * /usr/bin/python3 /opt/fetch-cortex-alerts.py") | crontab -
+systemctl enable filebeat
+systemctl start filebeat
